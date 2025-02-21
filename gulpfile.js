b21dd755b193d2413ce3e12e3d8fa3bab40c7016
Tigran <144844684+tigrankostanyan88@users.noreply.github.com>
2025-02-21 11:52:34 -0800

    Update gulpfile.js
    Updating outdated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,91 +1,106 @@
-import gulp from "gulp";
-import concat from "gulp-concat";
-import autoPrefixer from "gulp-autoprefixer";
-import cleanCSS from "gulp-clean-css";
-import dartSass from "sass";
-import gulpSass from "gulp-sass";
-import uglify from "gulp-uglify";
-import { deleteSync } from "del";
-import browserSync from "browser-sync";
-import imagemin from "gulp-imagemin";
-import gcmq from "gulp-group-css-media-queries";
-import sourcemaps from "gulp-sourcemaps";
-import babel from "gulp-babel";
+import gulp from 'gulp';
+import concat from 'gulp-concat';
+import autoPrefixer from 'gulp-autoprefixer';
+import cleanCSS from 'gulp-clean-css';
+import * as sass from 'sass';
+import gulpSass from 'gulp-sass';
+import uglify from 'gulp-uglify';
+import { deleteSync } from 'del';
+import browserSync from 'browser-sync';
+import imagemin from 'gulp-imagemin';
+import gcmq from 'gulp-group-css-media-queries';
+import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
 
-const sass = gulpSass(dartSass);
+// Initialize gulp-sass with the updated sass compiler
+const sassCompiler = gulpSass(sass);
 
+const paths = {
+  styles: {
+    src: './src/scss/main.scss',
+    dest: './build/css'
+  },
+  scripts: {
+    src: './src/js/**/*.js',
+    dest: './build/js'
+  },
+  images: {
+    src: './src/img/**/*',
+    dest: './build/img'
+  },
+  html: {
+    src: './src/*.html',
+    dest: './build/'
+  },
+  clean: './build/*'
+};
+
+// Styles task
 async function styles() {
-    return gulp.src("./src/scss/main.scss")
-      .pipe(sourcemaps.init())
-      .pipe(sass().on("error", sass.logError))  // Use gulp-sass here
-      .pipe(gcmq())
-      .pipe(concat("main.css"))
-      .pipe(autoPrefixer({
-        overrideBrowserslist: ["> 1%", "last 2 versions"],
-        cascade: false
-      }))
-      .pipe(cleanCSS({
-        level: 2
-      }))
-      .pipe(sourcemaps.write("."))
-      .pipe(gulp.dest("./build/css"))
-      .pipe(browserSync.stream());
-}
-
-async function scripts() {
-  return gulp.src("./src/js/**/*.js")
+  return gulp.src(paths.styles.src)
     .pipe(sourcemaps.init())
-    .pipe(concat("main.js"))
+    .pipe(sassCompiler().on('error', sassCompiler.logError))  // Use updated sass compiler
+    .pipe(gcmq())
+    .pipe(concat('main.css'))
+    .pipe(autoPrefixer({
+      overrideBrowserslist: ['> 1%', 'last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({ level: 2 }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+
+// Scripts task
+async function scripts() {
+  return gulp.src(paths.scripts.src)
+    .pipe(sourcemaps.init())
+    .pipe(concat('main.js'))
     .pipe(babel({
-      presets: ["@babel/env"]
+      presets: ['@babel/env']
     }))
-    .pipe(uglify({
-      toplevel: true
-    }))
-    .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("./build/js"))
+    .pipe(uglify({ toplevel: true }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest(paths.scripts.dest))
     .pipe(browserSync.stream());
 }
 
+// Image optimization task
 async function img() {
-  return gulp.src("./src/img/**/*")
-    .pipe(imagemin().on("error", function(err) {
-      console.error(err.message, '❌');
+  return gulp.src(paths.images.src)
+    .pipe(imagemin().on('error', function (err) {
+      console.error(err.message);
     }))
-    .pipe(gulp.dest("./build/img"));
+    .pipe(gulp.dest(paths.images.dest));
 }
 
-
+// Clean build directory
 async function clean() {
-  return deleteSync(["./build/*"]);
+  return deleteSync([paths.clean]);
 }
 
-async function fonts() {
-  return gulp.src("./src/fonts/*")
-    .pipe(gulp.dest("./build/fonts"));
-}
-
-async function htmls() {
-  return gulp.src("./src/*.html")
-    .pipe(gulp.dest("./build/"))
+// HTML task
+async function html() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
     .pipe(browserSync.stream());
 }
 
+// Watch task for live-reloading
 async function watch() {
   browserSync.init({
-    server: { baseDir: "./build" },
+    server: { baseDir: './build' },
     tunnel: false,
     port: 3200
   });
 
-  gulp.watch("./src/scss/**/*.scss", styles);
-  gulp.watch("./src/js/**/*.js", scripts);
-  gulp.watch("./src/*.html", htmls);
-  gulp.watch("./*.html").on("change", browserSync.reload);
+  gulp.watch('./src/scss/**/*.scss', styles);
+  gulp.watch('./src/js/**/*.js', scripts);
+  gulp.watch('./src/*.html', html);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 }
 
-gulp.task("watch", watch);
-
-gulp.task("build", gulp.series(clean, gulp.parallel(htmls, styles, scripts, img, fonts)));
-
-gulp.task("dev", gulp.series("build", "watch"));
+// Gulp tasks
+gulp.task('build', gulp.series(clean, gulp.parallel(html, styles, scripts, img)));
+gulp.task('dev', gulp.series('build', watch));
